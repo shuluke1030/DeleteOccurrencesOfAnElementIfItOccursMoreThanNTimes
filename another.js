5b37aaa95f132f82b913f2d56e19cafd00bfcd39
shuluke <shuluke1030@gmail.com>
2023-05-26 17:08:46 +0800

    other

--- another.js
+++ another.js
@@ -1,18 +1,18 @@
-function deleteNth(arr,n) {
-    let count = 0
+function deleteNth(arr, n) {
     let newArr = []
-    let test;
-    for(let i = 0 ; i < arr.length; i++) { 
-        test = arr[i]
-        for(let j = 0 ; j <= i; j++) { 
-            if(arr[j] == test) { 
+    for (let i = 0; i < arr.length; i++) {
+        let count = 0
+        let test = arr[i]
+        for (let j = 0; j <= i; j++) {
+            if (arr[j] == test) {
                 count++
             }
         }
-        if(count <= n) { 
+        if (count <= n) {
             newArr.push(arr[i])
         }
-        count = 0
+
     }
     return newArr
-  }
+}
+console.log(deleteNth([20, 37, 20, 21], 1))
